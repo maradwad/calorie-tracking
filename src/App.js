4daa2ce82d60d04cb0674e4b5e039281420b368b
Keshav7101 <Ironman@710>
2023-06-29 15:09:35 +0530

    Added Resources Folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import backgroundImg from "./resources/Background-Image.jpg" 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Calorie Tracker
-        </p>
-        <button>Track</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ 
+      backgroundImage: `url("https://media.geeksforgeeks.org/wp-content/uploads/rk.png")` 
+    }}>
+      Hello World
     </div>
   );
 }

7e8abd3ee05a629d9590c3b35008ac78a9d17b49
maradwad <101542302+maradwad@users.noreply.github.com>
2023-06-29 12:26:23 +0530

    Added intial changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Calorie Tracker
         </p>
         <a
           className="App-link"
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
